e9e2bd21a5712a6a6deed8ebe9205f421ddbb64c
AlexKross <alexandriusii@gmail.com>
2022-11-03 08:28:59 +0200

    Auth is working :)

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
  <?php 
-  include('dbconnect.php');
+
+ session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+    session_write_close();
+     require_once 'dbconnect.php';
+} else {
+    // since the username is not set in session, the user is not-logged-in
+    // he is trying to access this page unauthorized
+    // so let's clear all session variables and redirect him to index
+    session_unset();
+    session_write_close();
+    $url = "./login.php";
+    header("Location: $url");
+}
+
    //Code for deletion
     if(isset($_GET['delid']))
     {
